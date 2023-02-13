1a8a58e3ff42488fd705dc745b07287f6392f3e2
Bob Painter <kuroppi@protonmail.com>
2023-02-12 22:10:57 -0800

    Completed

---
+++ js/script.js
@@ -0,0 +1,162 @@
+// STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+// 5.0 // Numeric Literal Expression
+// 'Hello World' // String Literal Expression
+// true // Boolean Literal Expression
+// null // null Literal Expression
+
+// STEP 3
+// let totalMilesWalked = 2.5 * (2 + 3) + 5;
+
+// let milesSat = 5;
+// let milesSun = 10;
+// let totalMilesRun = milesSat + milesSun;
+
+// STEP 4
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let strZipCode;
+// let intYourAge;
+// let strReferralSource;
+// let strMayWeContactYou;
+
+// STEP 5
+// let strFirstName = "John";
+// var strLastName = "Doe";
+// const strAddress = "555 Main Street";
+
+// STEP 6
+// var theNumber = 7 + "7";
+// console.lg(theNumber);
+
+// STEP 7
+// let x = true + " blue";
+// let y = 5 + false;
+// console.log(x);
+// console.log(y);
+
+// STEP 8
+// Not valid. A \ needs to be added before the ' to escape it.
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// STEP 9
+// let x = null;
+// console.log(x);
+
+// let y;
+// console.log(y);
+
+// STEP 10
+
+// string
+// let myString = "thunder";
+// console.log(typeof myString);
+
+// number
+// let myNumber = 10;
+// console.log(typeof myNumber);
+
+// Boolean
+// let myResult = true;
+// console.log(typeof myResult);
+
+// object
+// let myObject = new Object();
+// console.log(typeof myObject);
+
+// undefined
+// let myResult;
+// console.log(typeof myResult);
+
+// STEP 11
+// alert('Hello ' + 'Bob Painter' + ', welcome to the JavaScript class!');
+
+// STEP 12
+// let name = 'Bob Painter';
+// alert('Hello ' + name + ', welcome to the JavaScript class!');
+
+// STEP 13
+// let name = 'Bob Painter';
+// let course = 'JavaScript';
+// alert('Hello ' + name + ', welcome to the ' + course + ' class!');
+
+// STEP 14
+// let name = 'Bob Painter';
+// let course = 'JavaScript';
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 15
+// let name = prompt('What is your name?');
+// let course = 'JavaScript';
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 16
+// let name = prompt('What is your name?');
+// let course = prompt('What class are you taking?');
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+// STEP 18
+// let x = 20;
+// x += 20;
+// console.log(x);
+
+// STEP 19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+// STEP 20
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x);
+
+// STEP 21
+// let x = 10;
+// let y = 15;
+
+// if (x < y && x + y === 25) {
+//   console.log(true);
+// }
+
+// STEP 22
+// var x = 20;
+// var y = 5;
+// var result = false;
+
+// if (x * y > 100 || x % y === 1) {
+//     result = true;
+// }
+
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
